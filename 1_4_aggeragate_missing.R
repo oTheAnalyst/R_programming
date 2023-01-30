b25a79fa202f26a7c0d252a435eadaaea59548c1
Vincent Cumbo <30744769+thealmanac1@users.noreply.github.com>
2023-01-30 08:50:09 -0500

    Delete 1_4_aggeragate_missing.R

--- 1_4_aggeragate_missing.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-setwd("~/workbook/Ex_Files_Cleaning_Bad_Data_R/Exercise Files/1_4")
-employees <- read_csv("employees.csv")
-
-View(employees)
-sum(employees$Salary,na.rm = TRUE)
-mean(employees$Salary,na.rm = TRUE)
-max(employees$Salary,na.rm = TRUE)
-
