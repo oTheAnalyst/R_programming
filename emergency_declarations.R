bf54452881685da254c0cf63dfafa869e2c5279a
Vincent Cumbo <30744769+thealmanac1@users.noreply.github.com>
2023-02-09 17:16:05 -0500

    Update emergency_declarations.R

--- emergency_declarations.R
+++ emergency_declarations.R
@@ -5,6 +5,7 @@ library(DBI)
 library(ggplot2)
 library(dplyr)
 
+
 ##library(ggpubr)
 ##library(broom222)
 
@@ -37,7 +38,6 @@ ORDER by fy_declared DESC;")
 
 #build the data frames
 disaster_df <- dbReadTable(con, "us_disaster_declarations")
-query <- dbReadTable(con,res)
 type<- disaster_df %>% 
   count(incident_type)
 
@@ -60,6 +60,7 @@ boxplot(res$Number_of_Disasters_Declared)
 boxplot(type$n)
 
 #filter out outliers
+#outliers filters where the year 2020(for covid-19), and 2023(because it was incomplete collection of information) 
 res <- res %>% 
   filter(Number_of_Disasters_Declared < 4500,Year_Declared < 2023)
 
@@ -70,7 +71,7 @@ boxplot(res$Number_of_Disasters_Declared)
 View(res)
 
 #build an arrangment paired with mutaute in DPLYR library than pass it to ggplot and build the graph
-#this reorders the factors in the graph so its easier to break down and look at/
+#this reorders the factors in the graph so its easier to break down and look at
 type %>% 
   arrange(n) %>% 
   mutate(incident_type=factor(incident_type,levels=incident_type)) %>% 
@@ -85,6 +86,7 @@ hist(res$Number_of_Disasters_Declared)
 #build a standard GGPLOT since this graph is on a timelines no need to do anything to it
 res.graph <-ggplot(data = res,mapping = aes(x = Year_Declared,y=Number_of_Disasters_Declared))+
   geom_point(aes())+
+  geom_smooth(method = "lm",col="black")+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
   labs(title = "Number of Natural Disasters Declared by year",subtitle = 
          "By the federal government")
@@ -93,8 +95,23 @@ res.graph <- res.graph +geom_smooth(method = "lm",col="black")
 
 
 cor(res$Year_Declared,res$Number_of_Disasters_Declared)
+cor.test(res$Year_Declared,res$Number_of_Disasters_Declared)
 
 
+growth_rate = res %>% 
+  arrange()
+
+res_growth <- res %>% 
+  arrange(Year_Declared) %>% 
+  mutate(Diff_Year = Year_Declared -lag(Year_Declared),
+         Diff_Grow = Number_of_Disasters_Declared - lag(Number_of_Disasters_Declared),
+         Rate_Percent = (Diff_Grow/Diff_Year)/lag(Number_of_Disasters_Declared) *100)
+
+
+Average_Growth <- mean(res_growth$Rate_Percent, na.rm = TRUE)
 
 #disconnect from the database
 dbDisconnect(con)
+
+
+
